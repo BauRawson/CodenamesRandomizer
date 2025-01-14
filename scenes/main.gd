f125ba25aa848b113d1de502652c58cf08b52969
BauRawson <bautistarawson@gmail.com>
2025-01-14 16:22:29 -0300

    - Now we can switch colors

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,30 +1,29 @@
 extends Node
-
 class_name MapRandomizer
-
 # Constants for tile types
-enum TileType {RED, BLUE, NEUTRAL, BLACK}
-
+enum TileType {ONE, TWO, NEUTRAL, BLACK}
 # Board properties
 const BOARD_SIZE = 25  # 5x5 grid
 const NEUTRAL_TILES = 7
 const BLACK_TILES = 1
-
 # Color palette
 var colors: Dictionary = {
-	TileType.RED: Color("#e6904e"),
-	TileType.BLUE: Color("#4d65b4"),
-	TileType.NEUTRAL: Color("#9babb2"),
+	TileType.ONE: Color("#ed8099"),
+	TileType.TWO: Color("#4d65b4"),
+	TileType.NEUTRAL: Color("#fbb954"),
 	TileType.BLACK: Color("#313638")
 }
-
 # Reference to the tile scene
 @export var tile_scene: PackedScene
-var is_red_board: bool = false
+var is_red_board: bool = false # red = TEAM 1
 
+var team_one_tiles: Array = []
+var team_two_tiles: Array = []
 
 func _ready() -> void:
+	load_team_colors()
 	randomize_and_replace()
+	connect_color_switchers()
 
 # Called when the "Randomize" button is pressed
 func generate_new_map():
@@ -37,14 +36,16 @@ func generate_new_map():
 	
 	# 3. Create array with all tile types
 	var tiles = []
+	team_one_tiles = []
+	team_two_tiles = []
 	
 	# Add red tiles
 	for i in range(red_tiles):
-		tiles.append(TileType.RED)
+		tiles.append(TileType.ONE)
 	
 	# Add blue tiles
 	for i in range(blue_tiles):
-		tiles.append(TileType.BLUE)
+		tiles.append(TileType.TWO)
 	
 	# Add neutral tiles
 	for i in range(NEUTRAL_TILES):
@@ -67,6 +68,9 @@ func place_tiles(grid_container: GridContainer):
 	var map_data = generate_new_map()
 	var tiles = map_data.tiles
 	
+	team_one_tiles = []
+	team_two_tiles = []
+	
 	# Clear existing tiles if any
 	for child in grid_container.get_children():
 		child.queue_free()
@@ -76,8 +80,13 @@ func place_tiles(grid_container: GridContainer):
 		var tile = tile_scene.instantiate()
 		grid_container.add_child(tile)
 		
-		tile.set_color(colors[tiles[i]])
-
+		tile.set_color_rect_color(colors[tiles[i]])
+		if tiles[i] == TileType.ONE:
+			team_one_tiles.append(tile)
+		elif tiles[i] == TileType.TWO:
+			team_two_tiles.append(tile)
+		elif tiles[i] == TileType.BLACK:
+			tile.get_node("MarginContainer/Nisman").texture = load("res://assets/fiscal_bisman.png")
 
 # Optional: Helper function to get the starting team
 func get_starting_team(map_data: Dictionary) -> String:
@@ -105,8 +114,62 @@ func hide_are_you_sure() -> void:
 
 func paint_color_indicators() -> void:
 	if is_red_board:
-		$TopColorRect.color = colors[TileType.RED]
-		$BottomColorRect.color = colors[TileType.RED]
+		$TopColorRect.color = colors[TileType.ONE]
+		$BottomColorRect.color = colors[TileType.ONE]
 	else:
-		$TopColorRect.color = colors[TileType.BLUE]
-		$BottomColorRect.color = colors[TileType.BLUE]
+		$TopColorRect.color = colors[TileType.TWO]
+		$BottomColorRect.color = colors[TileType.TWO]
+
+
+func change_team_color(team: TileType, new_color: Color) -> void:
+	colors[team] = new_color
+	save_team_colors()
+	adjust_team_colors()
+	
+	if team == TileType.ONE:
+		for tile in team_one_tiles:
+			tile.set_color_rect_color(colors[team])
+	elif team == TileType.TWO:
+		for tile in team_two_tiles:
+			tile.set_color_rect_color(colors[team])
+	
+	paint_color_indicators()
+
+
+func show_settings() -> void:
+	adjust_team_colors()
+	$Settings.show()
+
+
+func hide_settings() -> void:
+	$Settings.hide()
+
+
+func adjust_team_colors() -> void:
+	$Settings/Panel/MarginContainer/VBoxContainer/Team1Label.modulate = colors[TileType.ONE]
+	$Settings/Panel/MarginContainer/VBoxContainer/Team2Label.modulate = colors[TileType.TWO]
+
+
+func save_team_colors() -> void:
+	var config = ConfigFile.new()
+	config.set_value("colors", "team_one", colors[TileType.ONE].to_html())
+	config.set_value("colors", "team_two", colors[TileType.TWO].to_html())
+	config.save("user://team_colors.cfg")
+
+
+func load_team_colors() -> void:
+	var config = ConfigFile.new()
+	var err = config.load("user://team_colors.cfg")
+	if err == OK:
+		var team_one_color = config.get_value("colors", "team_one", colors[TileType.ONE].to_html())
+		var team_two_color = config.get_value("colors", "team_two", colors[TileType.TWO].to_html())
+		colors[TileType.ONE] = Color(team_one_color)
+		colors[TileType.TWO] = Color(team_two_color)
+
+
+func connect_color_switchers() -> void:
+	for tile in $Settings/Panel/MarginContainer/VBoxContainer/HBoxContainer.get_children():
+		tile.color_switched.connect(change_team_color.bind(TileType.ONE, tile.current_color))
+	
+	for tile in $Settings/Panel/MarginContainer/VBoxContainer/HBoxContainer2.get_children():
+		tile.color_switched.connect(change_team_color.bind(TileType.TWO, tile.current_color))
--- scenes/tile.gd
+++ scenes/tile.gd
@@ -1,8 +1,25 @@
 class_name Tile extends ColorRect
 
+signal color_switched
+@export var is_color_switcher: bool = false
 var checked: bool = false
+var current_color: Color
+
+
+func _ready() -> void:
+	current_color = color
+
 
 func on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		checked = !checked
-		$Highlight.visible = checked
+		if is_color_switcher:
+			print(current_color)
+			color_switched.emit()
+		else:
+			checked = !checked
+			$Highlight.visible = checked
+
+
+func set_color_rect_color(p_color: Color) -> void:
+	self.color = p_color
+	current_color = p_color
